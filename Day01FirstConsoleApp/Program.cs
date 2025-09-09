11d9c446309853207acb88e9787781e6badda6eb
Weber Yang <weberyang@build-school.com>
2025-09-09 16:24:39 +0800

    project init

---
+++ Day01FirstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day01FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.ReadLine();
+            //Console.WriteLine("My name is XXX");
+            Console.Write("Hello World!");
+            Console.Write("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day01MultiplicationTable/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Day01MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
